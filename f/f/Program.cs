4dc25d133f03a1c6abb5e8f7a1127c249ad05cd7
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-13 10:39:28 -0300

    commit f)

---
+++ f/f/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace f
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[,] Matriz = new int[3, 3];
+			Random numeros = new Random();
+			
+			// Preenchendo o vetor com números aleatórios
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					Matriz[i, j] = numeros.Next(1, 101); // Definindo entre 1 e 100
+				}
+			}
+			
+			// Matriz preenchida
+			Console.WriteLine("Matriz 3x3:");
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write(Matriz[i, j] + "\t");
+				}
+				
+				Console.WriteLine(); 
+			}
+			
+			// Acha o maior valor da matriz
+			int maior = 0;
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (Matriz[i, j] > maior)
+					{
+						maior = Matriz[i, j];
+					}
+				}
+			}
+			
+			Console.WriteLine("\nO maior valor da matriz é: {0}", maior);
+			Console.ReadKey(true);
+		}
+	}
+}
