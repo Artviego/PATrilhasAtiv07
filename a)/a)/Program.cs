92fa12876648373eb2ff10e9d12f0266e29732e5
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-13 08:17:16 -0300

    commit a)

---
+++ a)/a)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Matriz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[,] Matriz = new int[3, 3]
+			{
+				{ 1, 2, 3},
+				{ 4, 5, 6},
+				{7, 8, 9}
+			};
+			
+			// Exibição da matriz
+			Console.WriteLine("Matrix 3x3");
+			
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write(Matriz[j, i] + " ");
+				}
+				Console.WriteLine();
+			}
+			
+			Console.ReadKey(true);
+		}
+	}
+}
