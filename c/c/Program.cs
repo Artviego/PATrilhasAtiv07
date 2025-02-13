2d61387d8acb02e7ad0d996a614fdc0bfbf361d8
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-13 09:13:58 -0300

    commit c)

---
+++ c/c/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace c
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int[,] Matriz = new int[4, 2];
+			Random numeros = new Random();
+			int soma = 0;
+			
+			// Preenchendo os valores da matriz
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					Matriz[i, j] = numeros.Next(1, 11); // Números aleatórios entre 1 e 100
+				}
+			}
+			
+			// Impressão da matriz
+			Console.WriteLine("Matriz 4x2:");
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					Console.Write(Matriz[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+			
+			// Calcula a soma de todos os valores da matriz
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					soma += Matriz[i, j];
+				}
+			}
+			
+			// Exibe a soma
+			Console.WriteLine("Soma de todos os valores da matriz: {0}", soma);
+			Console.ReadKey(true);
+		}
+	}
+}
