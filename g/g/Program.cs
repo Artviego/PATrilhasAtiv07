b70e7a7d6ede74a84af38ee5af3313872889319e
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-13 10:39:48 -0300

    commit g)

---
+++ g/g/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    	int[,] Matriz = new int[4, 4]  // Preenche a Matriz
+    	{
+    		{ 1, 2, 3, 4 },
+    		{ 5, 6, 7, 8 },
+    		{ 9, 10, 11, 12},
+    		{ 13, 14, 15, 16}
+    	};
+    	
+    	Console.WriteLine("Números pares do vetor: ");
+    	// Exibe a matriz normal
+    	for (int i = 0; i < 4; i++)
+    	{
+    		for (int j = 0; j < 4; j++)
+    		{
+    			if (Matriz[i, j] % 2 == 0)
+    			{
+    				Console.WriteLine(Matriz[i, j] + " ");	
+    			}
+    		}
+    	}
+    	Console.WriteLine();
+   		Console.ReadKey(true);
+    }
+}
